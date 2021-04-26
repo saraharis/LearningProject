a9cbe7df6e18014c66527ebf08abd7c6ae4638b6
Sara <saraharis91@gmail.com>
2021-04-26 07:58:26 +0530

    new

--- src/main/java/MyLearningProject/restservice/WebSocketConfiguration.java
+++ src/main/java/MyLearningProject/restservice/WebSocketConfiguration.java
@@ -23,7 +23,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/sock").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/sock").setAllowedOriginPatterns("*").withSockJS();
     }
 
 
